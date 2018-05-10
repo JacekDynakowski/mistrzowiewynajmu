7191ca26a4d4099e18cf03a6b2ac793205f1340a
Jacek Dynakowski <Jacek@DESKTOP-0B745Q9>
2018-05-10 15:32:50 +0200

    Second Commit

---
+++ MistrzowieWynajmu/Models/Property.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MistrzowieWynajmu.Models
+{
+    public class Property
+    {
+        public int ID { get; set; }
+        public  PropertyType Type{ get; set; }
+
+        public String Description { get; set; }
+        public int Rooms { get; set; }
+        public int Area { get; set; }
+        public bool Washer { get; set; }
+        public bool Refrigerator { get; set; }
+        public bool Iron { get; set; }
+        public virtual int AddressId { get; set; }
+
+        public virtual Address Address { get; set; }
+        public virtual int  OwnerId { get; set; }
+        public virtual Owner Owner { get; set; }
+
+    }
+
+    public enum PropertyType
+    {
+        House=0,
+        Flat=1
+    }
+}
